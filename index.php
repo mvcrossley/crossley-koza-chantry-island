30446da1aa6ceaaa024f2dda388387b85464ad22
mvcrossley <mcrossl2@uwo.ca>
2017-02-08 15:01:40 -0500

    Making sure all files are up to date to start

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include("includes/header.html");?>
+
+    <section id="homebanner" class="row expanded">
+    <h2 class="hide">Welcome Banner</h2>
+      <div id="bannertext" class="small-11 medium-6 large-6 small-centered columns">
+        <h2>Discover Chantry Island...</h2>
+        <h3>Book your tour today!</h3>
+        <p>Stroll back in time with a visit to the Keeper’s Cottage, restored to it’s original 1800s condition, and decorated with period furnishing from privatedonors, and the Bruce County Museum and Cultural Center. Marvel at the wonders of nature with a walk through the protected bird sanctuary, and surrounding gardens.</p>
+        <a>Learn More</a>
+      </div>
+    </section>
+
+    <section id="islandIntro">
+      <h2>Learn More about the Island</h2>
+    </section>
+
+
+    <section id="aboutIntro">
+    <h2>Marine Heritage Society</h2>
+      
+    </section>
+
+    <section id="donateIntro">
+    <h2>Every Bit Counts</h2>
+      
+    </section>
+
+<?php include("includes/footer.html");?>
