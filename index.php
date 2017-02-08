31a67e497c7d78d490f3fea0551216e29db9a409
mvcrossley <mcrossl2@uwo.ca>
2017-02-08 15:54:50 -0500

    Updating Navs

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<?php include("includes/header.html");?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chantry Island | Home</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  
+  <body>
+    <?php include("includes/header.html");?>
 
     <section id="homebanner" class="row expanded">
     <h2 class="hide">Welcome Banner</h2>
@@ -25,4 +37,12 @@
       
     </section>
 
-<?php include("includes/footer.html");?>
+    <?php include("includes/footer.html");?>
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+    <script src="js/main.js"></script>    
+  </body>
+</html>
