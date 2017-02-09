697c30145c47ea9e0ac555400f6b7ba30d7fcfb0
mvcrossley <mcrossl2@uwo.ca>
2017-02-09 07:14:15 -0500

    One last time.

--- about.php
+++ about.php
@@ -11,6 +11,70 @@
   
   <body>
     <?php include("includes/header.html");?>
+
+    <section id="aboutbanner" class="sameLook row expanded">
+    <h2 class="hide">About Page Banner</h2>
+        <div class="sideImage small-12 medium-6 large-7 float-left columns" data-interchange="[images/mobdonbanner.jpg, small], [images/tabdonbanner.jpg, medium], [images/deskdonbanner.jpg, large]">
+        </div>
+
+        <div class="small-12 medium-6 large-5 float-right columns">
+            <h2 class="white center">The Marine Heritage Society</h2>
+            <p class="white">The Marine Heritage Society is a not-for-profit group of volunteers dedicated to the preservation and enhancement of our community's marine history. The objective of the Society is to identify, preserve and restore material items of marine historical significance and to raise sufficient funds to support these endeavors. They are also winner of the 2010 "Locals Know" Canada wide competition.</p>
+            <div class="center show-for-small-only"><a href="#bluehelp"><img class="downarrow" src="images/whitearrow.svg" alt="An arrow to go down a section"></a></div>
+        </div>
+    </section>
+
+    <section id="ourVolunteers" class="row">
+    <h2 class="hide">Our Volunteers Sections</h2>
+      <div class="small-12 medium-12 large-8 large-centered columns">
+        <h2 class="red center">Our Volunteers</h2>
+        <p class="center">Since so many people volunteer their time and energy to help our organization function, we have dedicated this section to our wonderful volunteers. Thank you all very much, this would not be possible without your time and donations.</p>
+      </div>
+    </section>
+
+    <section class="row">
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Don Nicholson - Chairman</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Pat O'Connor - Vice Chairman</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>John Rigby - Treasurer</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Stan Young - Secretary</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Rick Smith - Past Chairman</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Ali Kelly</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Jane Kramer</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Vicki Tomori</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Dan Holmes</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Dave Wenn</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Ed Braun</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>John Willetts</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 end columns">
+          <p>Peter Williamson - Observer</p>
+      </div>
+    </section>
+
+
     <?php include("includes/footer.html");?>
 
 
--- gallery.php
+++ gallery.php
@@ -11,12 +11,56 @@
   </head>
   
   <body>
+    <?php include("includes/header.html");?>
+
+    <section id="gallerybanner" class="sameLook row expanded">
+    <h2 class="hide">Gallery Page Banner</h2>
+        <div class="sideImage small-12 medium-6 large-7 float-left columns" data-interchange="[images/mobgalbanner.jpg, small], [images/tabgalbanner.jpg, medium], [images/deskgalbanner.jpg, large]">
+        </div>
+
+        <div class="small-12 medium-6 large-5 float-right columns">
+            <h2 class="white center">Photo Gallery</h2>
+            <p class="white">Welcome to our photo gallery. This section includes pictures of Chantry Island and the area of Lake Huron surrounding the Island. It also includes pictures of the lighthouse and keeper's cottage inside and out, as well as photos of some of the birds and flowers native to the island.</br>If you have photos of Chantry Island that you would like to share with us, connect with our Facebook page.</p>
+            <div class="center show-for-small-only"><a href="#bluehelp"><img class="downarrow" src="images/whitearrow.svg" alt="An arrow to go down a section"></a></div>
+        </div>
+    </section>
+
+    <section id="galleryPics" class="row">
+      <div id="thumbs" class="small-12 medium-8 medium-centered columns">
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb1.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb2.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb3.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb4.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb5.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb6.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb7.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb8.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb9.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb10.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb12.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb13.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb14.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb15.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column">
+          <img src="images/thumbs/thumb16.png" alt="thumb1"></div>
+        <div class="small-6 medium-2 column end">
+          <img src="images/thumbs/thumb17.png" alt="thumb1"></div>
+      </div>
+    </section>
 
 
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/vendor/what-input.js"></script>
-    <script src="js/vendor/foundation.js"></script>
-    <script src="js/app.js"></script>
-    <script src="js/main.js"></script>    
-  </body>
-</html>
+    <?php include("includes/footer.html");?>
