7604d797185e38206f33a79ebedb9cdf63fa1d4d
Moragh Crossley <m_crossley@fanshaweonline.ca>
2017-02-09 00:43:57 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <h2 class="white center">Discover Chantry Island...</h2>
         <h3 class="white center">Book your tour today!</h3>
         <p class="white">Stroll back in time with a visit to the Keeper’s Cottage, restored to it’s original 1800s condition, and decorated with period furnishing from privatedonors, and the Bruce County Museum and Cultural Center. Marvel at the wonders of nature with a walk through the protected bird sanctuary, and surrounding gardens.</p>
-        <div class="center"><a href="tours.php"><button class="whiteBut white center">Learn More</button></a></div>
+        <div class="center"><a class="whiteBut white center" href="tours.php">Learn More</a></div>
         <div class="center"><a href="#islandIntro"><img class="downarrow" src="images/whitearrow.svg"></a></div>
       </div>
     </section>
@@ -30,8 +30,11 @@
 
         <div class="small-12 medium-6 large-5 float-right columns">
             <h2 class="blue center">Learn more about the Island</h2>
-            <p>Chantry Island is located on Lake Huron, just over a mile Southwest of the Saugeen River, in Southampton, Ontario. Overlooking the island is a majestic Imperial Lighthouse built in the mid 1800s, accompanied by the Keeper’s Cottage, and boat house. In 1822, during a hydrographic survey, Captain Henry Bayfield of the Royal Navy, christened the island and named it after his friend and British Sculptor, Sir Francis Chantry.</p>
-            <div class="center"><a href="island.php"><button class="redBut white center">Learn More</button></a></div>
+            <p>Chantry Island is located on Lake Huron, just over a mile Southwest of the Saugeen River, in Southampton, 
+                Ontario. Overlooking the island is a majestic Imperial Lighthouse built in the mid 1800s, accompanied by the Keeper’s 
+                Cottage, and boat house. In 1822, during a hydrographic survey, Captain Henry Bayfield of the Royal Navy, christened the island 
+                and named it after his friend and British Sculptor, Sir Francis Chantry.</p>
+            <div class="center"><a class="redBut white center" href="island.php">Learn More</a></div>
         </div>
     </section>
 
@@ -43,7 +46,7 @@
         <div id="heritage" class="small-12 medium-6 float-left large-5 columns">
             <h2 class="red center">The Marine Heritage Society</h2>
             <p>Get to know us and what we do! Stay up to date on news, events, volunteers, and important dates!</p>
-            <div class="center"><a href="island.php"><button class="blueBut white center">Learn More</button></a></div>
+            <div class="center"><a href="island.php" class="blueBut white center">Learn More</a></div>
           </div>
         </div>
     </section>
@@ -56,7 +59,7 @@
         <div id="donation" class="small-12 medium-6 large-5 float-right columns">
             <h2 class="blue center">Every Bit Counts.</h2>
             <p>When it comes to donations, every little bit counts. Here at the Marine Heritage Society, we are determined to make our one-of-a-kind tours the very best, Click the link below to learn how you can support Chantry Island.</p>
-            <div class="center"><a href="island.php"><button class="redBut white center">Learn More</button></a></div>
+            <div class="center"><a href="island.php" class="redBut white center">Learn More</a></div>
         </div>
     </section>
       
