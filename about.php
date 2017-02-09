ab284eb9e8387fdf3947be5da508ab928dad4be5
Moragh Crossley <m_crossley@fanshaweonline.ca>
2017-02-09 01:20:57 -0500

    Add files via upload

--- about.php
+++ about.php
@@ -7,16 +7,10 @@
     <title>Chantry Island | About Us</title>
     <link rel="stylesheet" href="css/foundation.css">
     <link rel="stylesheet" href="css/app.css">
-
   </head>
   
   <body>
+    <?php include("includes/header.html");?>
+    <?php include("includes/footer.html");?>
 
 
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/vendor/what-input.js"></script>
-    <script src="js/vendor/foundation.js"></script>
-    <script src="js/app.js"></script>
-    <script src="js/main.js"></script>    
-  </body>
-</html>
--- island.php
+++ island.php
@@ -11,12 +11,21 @@
   </head>
   
   <body>
+    <?php include("includes/header.html");?>
 
+     <section id="islandBanner" class="sameLook row expanded">
+      <h2 class="hide">An Intro to The Island</h2>
+          <div class="sideImage small-12 medium-6 large-7 float-left columns" data-interchange="[images/mobaerial.jpg, small], [images/tabaerial.jpg, medium], [images/deskaerial.jpg, large]">
+          </div>
 
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/vendor/what-input.js"></script>
-    <script src="js/vendor/foundation.js"></script>
-    <script src="js/app.js"></script>
-    <script src="js/main.js"></script>    
-  </body>
-</html>
+          <div class="small-12 medium-6 large-5 float-right columns">
+              <h2 class="white center">About Chantry Island...</h2>
+              <p class="white">Chantry Island is located on Lake Huron, just over a mile Southwest of the Saugeen River, in Southampton, 
+                  Ontario. Overlooking the island is a majestic Imperial Lighthouse built in the mid 1800s, accompanied by the Keeper’s 
+                  Cottage, and boat house. In 1822, during a hydrographic survey, Captain Henry Bayfield of the Royal Navy, christened the island 
+                  and named it after his friend and British Sculptor, Sir Francis Chantry.</p>
+              <div class="center show-for-small-only"><a href="#islandIntro"><img class="downarrow" src="images/whitearrow.svg"></a></div>
+          </div>
+      </section>
+
+    <?php include("includes/footer.html");?>
