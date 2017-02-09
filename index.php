90bc2278583ba2513d9d3b61287c3c3eaf3caf92
mvcrossley <mcrossl2@uwo.ca>
2017-02-08 22:24:30 -0500

    Homepage complete

--- index.php
+++ index.php
@@ -12,28 +12,52 @@
   <body>
     <?php include("includes/header.html");?>
 
-    <section id="homebanner" class="row expanded">
+    <section id="homebanner" class="row expanded" data-interchange="[images/mobisland2.jpg, small], [images/tabisland2.jpg, medium], [images/island2.jpg, large]">
     <h2 class="hide">Welcome Banner</h2>
-      <div id="bannertext" class="small-11 medium-6 large-6 small-centered columns">
-        <h2>Discover Chantry Island...</h2>
-        <h3>Book your tour today!</h3>
-        <p>Stroll back in time with a visit to the Keeper’s Cottage, restored to it’s original 1800s condition, and decorated with period furnishing from privatedonors, and the Bruce County Museum and Cultural Center. Marvel at the wonders of nature with a walk through the protected bird sanctuary, and surrounding gardens.</p>
-        <a>Learn More</a>
+      <div id="bannertext" class="small-11 medium-8 large-6 small-centered columns">
+        <h2 class="white center">Discover Chantry Island...</h2>
+        <h3 class="white center">Book your tour today!</h3>
+        <p class="white">Stroll back in time with a visit to the Keeper’s Cottage, restored to it’s original 1800s condition, and decorated with period furnishing from privatedonors, and the Bruce County Museum and Cultural Center. Marvel at the wonders of nature with a walk through the protected bird sanctuary, and surrounding gardens.</p>
+        <div class="center"><a href="tours.php"><button class="whiteBut white center">Learn More</button></a></div>
+        <div class="center"><a href="#islandIntro"><img class="downarrow" src="images/whitearrow.svg"></a></div>
       </div>
     </section>
 
-    <section id="islandIntro">
-      <h2>Learn More about the Island</h2>
+    <section id="islandIntro" class="sameLook row expanded">
+    <h2 class="hide">An Intro to The Island</h2>
+        <div class="sideImage small-12 medium-6 large-7 columns" data-interchange="[images/mobshore.jpg, small], [images/tabshore.jpg, medium], [images/deskshore.jpg, large]">
+        </div>
+        <div class="small-12 medium-6 large-5 columns">
+            <h2 class="blue center">Learn more about the Island</h2>
+            <p>Chantry Island is located on Lake Huron, just over a mile Southwest of the Saugeen River, in Southampton, Ontario. Overlooking the island is a majestic Imperial Lighthouse built in the mid 1800s, accompanied by the Keeper’s Cottage, and boat house. In 1822, during a hydrographic survey, Captain Henry Bayfield of the Royal Navy, christened the island and named it after his friend and British Sculptor, Sir Francis Chantry.</p>
+            <div class="center"><a href="island.php"><button class="redBut white center">Learn More</button></a></div>
+            <div class="center hide-for-medium"><a href="#aboutIntro"><img class="downarrow" src="images/blackarrow.svg"></a></div>
+        </div>
     </section>
 
+    <section id="aboutIntro" class="sameLook row expanded">
+    <h2 class="hide">An Intro to The Society</h2>
+        <div id="heritage" class="small-12 medium-6 large-5 columns">
+            <h2 class="red center">The Marine Heritage Society</h2>
+            <p>Get to know us and what we do! Stay up to date on news, events, volunteers, and important dates!</p>
+            <div class="center"><a href="island.php"><button class="blueBut white center">Learn More</button></a></div>
+            <div class="center hide-for-medium"><a href="#donateIntro"><img class="downarrow" src="images/blackarrow.svg"></a></div>
+        </div>
 
-    <section id="aboutIntro">
-    <h2>Marine Heritage Society</h2>
-      
+        <div class="sideImage small-12 medium-6 large-7 columns" data-interchange="[images/mobbase.jpg, small], [images/tabbase.jpg, medium], [images/deskbase.jpg, large]">
+        </div>
     </section>
 
-    <section id="donateIntro">
-    <h2>Every Bit Counts</h2>
+    <section id="donateIntro" class="sameLook row expanded">
+    <h2 class="hide">An Into to Donation</h2>
+        <div class="sideImage small-12 medium-6 large-7 columns" data-interchange="[images/mobdonate.jpg, small], [images/tabdonate.jpg, medium], [images/deskdonate.jpg, large]">
+        </div>
+        <div id="donation" class="small-12 medium-6 large-5 columns">
+            <h2 class="blue center">Every Bit Counts.</h2>
+            <p>When it comes to donations, every little bit counts. Here at the Marine Heritage Society, we are determined to make our one-of-a-kind tours the very best, Click the link below to learn how you can support Chantry Island.</p>
+            <div class="center"><a href="island.php"><button class="redBut white center">Learn More</button></a></div>
+        </div>
+    </section>
       
     </section>
 
