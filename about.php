2585dd382ac87a9c7d7e89b2695dda5ade6a5483
Moragh Crossley <m_crossley@fanshaweonline.ca>
2017-02-09 11:36:20 -0500

    Update about.php

--- about.php
+++ about.php
@@ -23,6 +23,56 @@
             <div class="center show-for-small-only"><a href="#bluehelp"><img class="downarrow" src="images/whitearrow.svg" alt="An arrow to go down a section"></a></div>
         </div>
     </section>
+    
+    <section id="ourVolunteers" class="row">
+    <h2 class="hide">Our Volunteers Sections</h2>
+      <div class="small-12 medium-12 large-8 large-centered columns">
+        <h2 class="red center">Our Volunteers</h2>
+        <p class="center">Since so many people volunteer their time and energy to help our organization function, we have dedicated this section to our wonderful volunteers. Thank you all very much, this would not be possible without your time and donations.</p>
+      </div>
+    </section>
+
+    <section class="row">
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Don Nicholson - Chairman</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Pat O'Connor - Vice Chairman</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>John Rigby - Treasurer</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Stan Young - Secretary</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Rick Smith - Past Chairman</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Ali Kelly</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Jane Kramer</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Vicki Tomori</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Dan Holmes</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Dave Wenn</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>Ed Braun</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 columns">
+          <p>John Willetts</p>
+      </div>
+      <div class="volunteers center small-8 medium-4 large-3 end columns">
+          <p>Peter Williamson - Observer</p>
+      </div>
+    </section>
 
 
     <section id="volVids" class="row">
