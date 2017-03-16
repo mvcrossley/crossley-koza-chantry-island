905e82fc45b46639497084c3c0644a9dbe05e466
laurenkoza <l_koza3@fanshaweonline.ca>
2017-03-15 22:04:32 -0400

    Trying to get directions to work

--- js/maps.js
+++ js/maps.js
@@ -2,48 +2,50 @@
 
     var map = new google.maps.Map(document.querySelector('#map'));
     var marker;
-    var geocoder = new google.maps.Geocoder(); //from https://developers.google.com/maps/documentation/javascript/
-    var geocodeButton = document.querySelector(".geocode");
+    var directionButton = document.querySelector("#get-directions");
 
         //directions service - draw a route on a map
     var directionService = new google.maps.DirectionsService();
-    var locations=[];
     var directionsDisplay;
+    var locations = [];
 
-    function iniMap(position) {
-        locations[0] = { lat: position.coords.latitude, lng: position.coords.longitude }; /*locations is an array that stores locations*/
 
-        directionsDisplay = new google.maps.DirectionsRenderer();
-        directionsDisplay.setMap(map);//tells it to show itself on "map" map, as defined below in codeAddress
+    
 
+    function initMap() {
+
+        locations[0] = { lat: 44.500026, lng: -81.373138 };
+        console.log('helloooo');
         //Map is centered over Marine Hertiage Society
         map.setCenter({ lat: 44.500026, lng: -81.373138 });
-        map.setZoom(14);
+        map.setZoom(13);
 
-        //Chantry Island Marker
-        marker = new google.maps.Marker({
-            //position: { lat: position.coords.latitude, lng: position.coords.longitude },
-            position: { lat: 44.491419, lng: -81.404225 },
-            map: map,
-            title: "Chantry Island"
-        });
         //Marine Heritage Marker
         marker = new google.maps.Marker({
-            //position: { lat: position.coords.latitude, lng: position.coords.longitude },
             position: { lat: 44.500026, lng: -81.373138 },
             map: map,
             title: "Marine Heritage Society"
         });
+
+        //Chantry Island Marker
+        marker = new google.maps.Marker({
+            position: { lat: 44.491419, lng: -81.404225 },
+            map: map,
+            title: "Chantry Island"
+        });
     }
 
-    //geocoding api => find and address on a map
+    function getDirections(position){
+        directionsDisplay = new google.maps.DirectionsRenderer();
+        directionsDisplay.setMap(map);//tells it to show itself on "map" map, as defined below in codeAddress
 
-    function codeAddress(){
-        var address = document.querySelector('.address').value;
+        var address = document.querySelector('#address').value;
 
         geocoder.geocode( {'address' : address}, function(results,status){
             if (status === google.maps.GeocoderStatus.OK){//making sure it loads
-                locations[1] = { lat: results[0].geometry.location.lat(), /*create second location*/ lng: results[0].geometry.location.lng() };
+                locations[1] = { lat: results[0].geometry.location.lat(), //create second location
+                                lng: results[0].geometry.location.lng() };
+
                 map.setCenter(results[0].geometry.location);
 
                 if(marker){
@@ -54,7 +56,7 @@
                     });
                 }
 
-            calcRoute(results[0].geometry.location);//make up function calcRoute and pass data through
+                calcRoute(results[0].geometry.location);//make up function calcRoute and pass data through
 
             } else {
                 console.log('Geocoder was not successful for the following reason:',status);
@@ -79,10 +81,33 @@
         });
     }
 
+    /*function getDirections(position){
+        directionsDisplay = new google.maps.DirectionsRenderer();
+       
+        var request = {
+            origin: 'London,ON'//locations[1],
+            destination: //locations[0],
+            travelMode: 'DRIVING'
+        };
+        
+        directionsDisplay.setMap(map);
+        directionService.route(request, function(result,status){
+            if(status==='OK'){
+                directionsDisplay.setDirections(result);
+            }
+        });
 
+        /*marker = new google.maps.Marker({
+            position: { lat: position.coords.latitude, lng: position.coords.longitude },
+            //position: { lat: 44.500026, lng: -81.373138 },
+            map: map,
+            title: "Marine Heritage Society"
+        });
+    }*/
 
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(iniMap, handleError);
+        //navigator.geolocation.getCurrentPosition(initMap, handleError);
+        navigator.geolocation.getCurrentPosition(getDirections, handleError);
     }else{ //give some kind of error message to the user
         console.log('Your browser does not have a geolocation');
     }
@@ -91,7 +116,9 @@
         console.log(e);
     }
 
-    //geocodeButton.addEventListener ('click', codeAddress, false);
 
-    //iniMap();//fires map function
+
+    directionButton.addEventListener ('click', getDirections, false);
+
+    //initMap();//fires map function
 })();
