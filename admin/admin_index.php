d26c98b92c524480cedf59889ba1d29374cb75b8
mvcrossley <mcrossl2@uwo.ca>
2017-04-05 00:00:38 -0400

    more cms

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -14,8 +14,25 @@
 </head>
 
 <body>
-	<section class"adminban row expanded" data-interchange="[../images/mobisland2.jpg, small], [../images/tabisland2.jpg, medium], [../images/island2.jpg, large]">
+	<section class"adminban row expanded" data-interchange="[../images/admin_mob.jpg, small], [../images/admin_tab.jpg, medium], [../images/admin_desk.jpg, large]">
 		<h2 class="hide">Admin Banner Section</h2>
+	</section>
+
+	<section class="row expanded">
+		<h2 class="hide">Main Section</h2>
+		
+		<div class="titlepanel small-12 medium-4 float-left columns">
+			<h2>Admin Island Home</h2>
+			<h3>Welcome to your admin panel (name), what would you like to do?</h3>
+		</div>
+
+		<div class="actionpanel small-12 medium-8 columns">
+			<ul>
+				<a href="admin_edituser.php"><li>Edit Member Profile</li></a>
+				<a href="admin_createuser.php"><li>Create a New Member</li></a>
+				<a href="admin_editcontent.php"><li>Edit Site Content</li></a>
+			</ul>
+		</div>
 	</section>
 </body>
 
